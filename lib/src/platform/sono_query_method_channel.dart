e251a4f054fdf1ffff0fc15caec1f005536ce18e
mathiiiiiis <mathis@mathiiis.de>
2026-03-08 18:51:26 +0100

    Add iOS and Android Method Channel

---
+++ lib/src/platform/sono_query_method_channel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/services.dart';
+import 'package:sono_query/src/platform/sono_query_platform.dart';
+
+class SonoQueryMethodChannel extends SonoQueryPlatform {
+  final _channel = const MethodChannel('sono_query');
+
+    @override
+    Future<List<String>> getAudioFilePaths() async {
+      final paths = await _channel.invokeListMethod<String>('getAudioFilePaths');
+      return paths ?? [];
+    }
+}
