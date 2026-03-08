13b37a36a31d8f1aac3e9de31dcc51daa2d909aa
mathiiiiiis <mathis@mathiiis.de>
2026-03-08 20:21:21 +0100

    ran dart format

--- lib/src/platform/sono_query_method_channel.dart
+++ lib/src/platform/sono_query_method_channel.dart
@@ -4,9 +4,9 @@ import 'package:sono_query/src/platform/sono_query_platform.dart';
 class SonoQueryMethodChannel extends SonoQueryPlatform {
   final _channel = const MethodChannel('sono_query');
 
-    @override
-    Future<List<String>> getAudioFilePaths() async {
-      final paths = await _channel.invokeListMethod<String>('getAudioFilePaths');
-      return paths ?? [];
-    }
+  @override
+  Future<List<String>> getAudioFilePaths() async {
+    final paths = await _channel.invokeListMethod<String>('getAudioFilePaths');
+    return paths ?? [];
+  }
 }
